714944eb6a102a5b830edaea552517110c92e871
Daniel Burkhart <hoschi-hammer@web.de>
2023-11-29 11:25:03 +0100

    fix: read only the title from the header and not all title tags

--- board-chrome-extension/content.js
+++ board-chrome-extension/content.js
@@ -1,5 +1,5 @@
 var oPageInfo = {
-    'title': jQuery(document).find("title").text(),
+    'title': jQuery(document).find("head>title").text(),
     'url': window.location.href
 };
 // Send a message containing the page details back to the event page
